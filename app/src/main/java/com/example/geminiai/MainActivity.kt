7569dd6e59987af132a4ed5477f2622583c587d9
oguzhanaslann <oguz21199@gmail.com>
2023-12-16 15:24:50 +0300

    - gemini gpt started :D

--- app/src/main/java/com/example/geminiai/MainActivity.kt
+++ app/src/main/java/com/example/geminiai/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.geminiai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,115 +46,150 @@ import com.google.ai.client.generativeai.GenerativeModel
 import com.example.geminiai.ui.theme.GeminiAITheme
 
 class MainActivity : ComponentActivity() {
+
+    private val generativeModel = GenerativeModel(
+        modelName = "gemini-pro",
+        apiKey = BuildConfig.apiKey
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GeminiAITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background,
-                ) {
-                    val generativeModel = GenerativeModel(
-                            modelName = "gemini-pro",
-                            apiKey = BuildConfig.apiKey
-                    )
-                    val viewModel = SummarizeViewModel(generativeModel)
-                    SummarizeRoute(viewModel)
-                }
+                val viewModel = SummarizeViewModel(generativeModel)
+                GeminiAIScreen(viewModel)
             }
         }
     }
 }
 
 @Composable
-internal fun SummarizeRoute(
-        summarizeViewModel: SummarizeViewModel = viewModel()
+internal fun GeminiAIScreen(
+    summarizeViewModel: SummarizeViewModel = viewModel()
 ) {
-    val summarizeUiState by summarizeViewModel.uiState.collectAsState()
 
-    SummarizeScreen(summarizeUiState, onSummarizeClicked = { inputText ->
-        summarizeViewModel.summarize(inputText)
-    })
+    val summarizeUiState by summarizeViewModel.uiState.collectAsState()
+    GeminiAIScreenView(
+        summarizeUiState,
+        onSummarizeClicked = { inputText ->
+            summarizeViewModel.summarize(inputText)
+        })
 }
 
 @Composable
-fun SummarizeScreen(
-        uiState: SummarizeUiState = SummarizeUiState.Initial,
-        onSummarizeClicked: (String) -> Unit = {}
+fun GeminiAIScreenView(
+    uiState: SummarizeUiState = SummarizeUiState.Initial,
+    onSummarizeClicked: (String) -> Unit = {}
 ) {
-    var prompt by remember { mutableStateOf("") }
-    Column(
-            modifier = Modifier
-                    .padding(all = 8.dp)
-                    .verticalScroll(rememberScrollState())
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
     ) {
-        Row {
-            TextField(
-                    value = prompt,
-                    label = { Text(stringResource(R.string.summarize_label)) },
-                    placeholder = { Text(stringResource(R.string.summarize_hint)) },
-                    onValueChange = { prompt = it },
-                    modifier = Modifier
-                            .weight(8f)
-            )
-            TextButton(
-                    onClick = {
-                        if (prompt.isNotBlank()) {
-                            onSummarizeClicked(prompt)
-                        }
-                    },
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp)
+                .padding(bottom = 16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
 
-                    modifier = Modifier
-                            .weight(2f)
-                            .padding(all = 4.dp)
-                            .align(Alignment.CenterVertically)
+            Column(
+                modifier = Modifier.align(Alignment.TopCenter)
             ) {
-                Text(stringResource(R.string.action_go))
-            }
-        }
-        when (uiState) {
-            SummarizeUiState.Initial -> {
-                // Nothing is shown
-            }
+                when (uiState) {
+                    SummarizeUiState.Initial -> {
+                        // Nothing is shown
+                        Column {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_geminiai),
+                                contentDescription = null
+                            )
+                            Text(
+                                text = stringResource(id = R.string.app_name),
+                                color = Color.White
+                            )
+                        }
+                    }
 
-            SummarizeUiState.Loading -> {
-                Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
+                    SummarizeUiState.Loading -> {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
                                 .padding(all = 8.dp)
                                 .align(Alignment.CenterHorizontally)
-                ) {
-                    CircularProgressIndicator()
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
+
+                    is SummarizeUiState.Success -> {
+                        Row(modifier = Modifier.padding(all = 8.dp)) {
+                            Icon(
+                                Icons.Outlined.Person,
+                                contentDescription = "Person Icon"
+                            )
+                            Text(
+                                text = uiState.outputText,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                        }
+                    }
+
+                    is SummarizeUiState.Error -> {
+                        Text(
+                            text = uiState.errorMessage,
+                            color = Color.Red,
+                            modifier = Modifier.padding(all = 8.dp)
+                        )
+                    }
                 }
             }
 
-            is SummarizeUiState.Success -> {
-                Row(modifier = Modifier.padding(all = 8.dp)) {
-                    Icon(
-                            Icons.Outlined.Person,
-                            contentDescription = "Person Icon"
-                    )
-                    Text(
-                            text = uiState.outputText,
-                            modifier = Modifier.padding(horizontal = 8.dp)
-                    )
-                }
-            }
 
-            is SummarizeUiState.Error -> {
-                Text(
-                        text = uiState.errorMessage,
-                        color = Color.Red,
-                        modifier = Modifier.padding(all = 8.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                val (text, onValueChanged) = remember { mutableStateOf("tate") }
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 48.dp)
+                        .heightIn(min = 56.dp)
+                        .align(Alignment.CenterStart),
+                    value = text,
+                    onValueChange = onValueChanged,
+                    shape = RoundedCornerShape(24.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF8c8375),
+                        unfocusedBorderColor = Color(0xFF343642),
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color(0xFF737576)
+                    )
                 )
+                IconButton(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.CenterEnd),
+                    onClick = { onSummarizeClicked(text) }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Send,
+                        contentDescription = "Send",
+                        tint = Color.White
+                    )
+                }
             }
         }
     }
 }
 
+
 @Composable
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 fun SummarizeScreenPreview() {
-    SummarizeScreen()
+    GeminiAITheme {
+        GeminiAIScreenView()
+    }
 }
--- app/src/main/java/com/example/geminiai/SummarizeViewModel.kt
+++ app/src/main/java/com/example/geminiai/SummarizeViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.geminiai
 
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,10 +19,8 @@ class SummarizeViewModel(
     val uiState: StateFlow<SummarizeUiState> =
             _uiState.asStateFlow()
 
-    fun summarize(inputText: String) {
+    fun summarize(prompt: String) {
         _uiState.value = SummarizeUiState.Loading
-
-        val prompt = "Summarize the following text for me: $inputText"
 
         viewModelScope.launch {
             try {
@@ -32,5 +32,28 @@ class SummarizeViewModel(
                 _uiState.value = SummarizeUiState.Error(e.localizedMessage ?: "")
             }
         }
+    }
+
+    fun generateContent(
+        prompt: String,
+        vararg images: Bitmap
+    ) {
+        _uiState.value = SummarizeUiState.Loading
+        viewModelScope.launch {
+            try {
+                val content = content {
+                    images.forEach(::image)
+                    text(prompt)
+                }
+
+                val response  = generativeModel.generateContent(content)
+                response.text?.let { outputContent ->
+                    _uiState.value = SummarizeUiState.Success(outputContent)
+                }
+            }  catch (e: Exception) {
+                _uiState.value = SummarizeUiState.Error(e.localizedMessage ?: "")
+            }
+        }
+
     }
 }
