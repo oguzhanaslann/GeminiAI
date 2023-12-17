1c1100467f2dc8b998c7648ba6d140c191affe95
oguzhanaslann <oguz21199@gmail.com>
2023-12-17 15:59:37 +0300

    - color changes

--- app/src/main/java/com/oguzhanaslann/geminiai/ChatUI.kt
+++ app/src/main/java/com/oguzhanaslann/geminiai/ChatUI.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -109,7 +110,7 @@ fun ChatList(
                     modifier = Modifier.padding(all = 8.dp)
                 )
             }.onLoading {
-                CircularProgressIndicator()
+                CircularProgressIndicator(color =MaterialTheme.colorScheme.onBackground)
             }.onSuccess {
                 when (it.sender) {
                     Sender.Bot -> BotChatMessageView(
